5591c796d597ba17373bc6a2006cc2f62959cd1b
dannyets <dannyets@gmail.com>
2019-07-11 16:15:41 +0300

    Fixed type warnings

--- 2. TypeScript/src/models/repository.ts
+++ 2. TypeScript/src/models/repository.ts
@@ -1,12 +1,12 @@
 import { Entity } from './entity';
 
 export interface Repository<T extends Entity> {
-    get: () => T[],
-    getById: (id: string) => T,
-    add: (entity: T) => T,
-    update: (entity : T) => unknown,
-    remove: (id: string) => unknown,
-    findEntityIndex: (id: string) => number,
-    isExists: (id: string) => boolean,
-    getFiltered: (filter: (value: T) => boolean) => T[]
+    get: () => T[];
+    getById: (id: string) => T;
+    add: (entity: T) => T;
+    update: (entity: T) => void;
+    remove: (id: string) => void;
+    findEntityIndex: (id: string) => number;
+    isExists: (id: string) => boolean;
+    getFiltered: (filter: (value: T) => boolean) => T[];
 }
