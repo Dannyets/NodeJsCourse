f7d6ddb8344ae5e427b7c70455160d4a8718b442
dannyets <dannyets@gmail.com>
2019-07-23 18:04:34 +0300

    Remove unnecessary folders

--- 1. Essentials/Exercises/1/index.js
+++
@@ -1,9 +0,0 @@
-const server = require('./server1');
-const client = require('./client');
-
-const port = 8000;
-const url = `http://localhost:${port}`;
-
-server.init(port);
-
-client.makeAsyncRequests(url, 10);
